ddd2f7d1b8dc5098400c3e4c06f8fdf4b7b5f53b
Valentin Pelloin <valentinpelloin@gmail.com>
2018-01-29 13:02:16 +0100

    Error tmux symbolic link

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ else
 	ln -s ~/git/myConfigs/.vimrc ~/.vimrc
 	ln -s ~/git/myConfigs/.vim ~/.vim
 	ln -s ~/git/myConfigs/.vimpkg ~/.vimpkg
-	ln -s ~/git/myConfigs/.tmux.conf
+	ln -s ~/git/myConfigs/.tmux.conf ~/.tmux.conf
 
 	echo " ... Done"
 
